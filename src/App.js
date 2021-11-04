f3cb5e3ec26bf7ff3c3b69d38331ea4ee1f31937
Vladosik1500 <90395342+Vladosik1500@users.noreply.github.com>
2021-11-04 16:43:48 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ]);
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+
+      <TodoList todos={todos} setTodos={setTodos} />
+      <AddTodo setTodos={setTodos} />
+    </div>
+  );
+}
+
+function TodoList({ todos, setTodos }) {
+  function handleToggleTodo(todo) {
+    const updateTodos = todos.map((t) =>
+      t.id === todo.id
+        ? {
+            ...t,
+            done: !t.done
+          }
+        : t
+    );
+    setTodos(updateTodos);
+  }
+
+  if (!todos.length) {
+    return <p> No todos left!</p>;
+  }
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li
+          onDoubleClick={() => handleToggleTodo(todo)}
+          style={{
+            textDecoration: todo.done ? "line-through" : ""
+          }}
+          key={todo.id}
+        >
+          {todo.text}
+          <DeleteTodo todo={todo} setTodos={setTodos} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function DeleteTodo({ todo, setTodos }) {
+  function handledeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
+  return (
+    <span
+      onClick={handledeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10
+      }}
+    >
+      x
+    </span>
+  );
+}
+
+function AddTodo({ setTodos }) {
+  const inputRef = React.useRef();
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    });
+    inputRef.current.value = "";
+  }
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add Todo" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
